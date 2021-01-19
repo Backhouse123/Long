27377f76202968958d2905d024cda7bbd08c1d40
hp <2570879265@qq.com>
2021-01-19 19:35:21 +0800

    1.0

---
+++ src/main/java/cn/kgc/controller/MainController.java
@@ -0,0 +1,29 @@
+package cn.kgc.controller;
+
+import cn.kgc.entity.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Controller
+public class MainController {
+    @RequestMapping("getMessage")
+    public String getMessage(Model model){
+        model.addAttribute("message","hello thymeleaf");
+        User user1 = new User(1,"张三","root",new Date());
+        User user2 = new User(2,"李四","root",new Date());
+        User user3 = new User(3,"王五","root",new Date());
+        User user4 = new User(4,"赵六","root",new Date());
+        List<User> userList = new ArrayList<>();
+        userList.add(user1);
+        userList.add(user2);
+        userList.add(user3);
+        userList.add(user4);
+        model.addAttribute("userList",userList);
+        return "main";
+    }
+}
